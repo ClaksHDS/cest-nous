9e8b660cc36ba20715949575224bb82d9cce88c7
[claks HDS] <you@example.com>
2023-01-19 16:25:49 +0000

    Contact component Home Page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,102 @@ import React from "react";
 import styled from "styled-components";
 
 function Contact() {
-  return <div>Contact</div>;
+  return (
+    <Wrapper>
+      <div className='section-center'>
+        <h3>Inscrivez-vous à notre newsletter</h3>
+        <div className='content'>
+          <p>
+            Pour être les premiers au courant de nos dernières créations et des
+            évènements auxquels vous pourrez nous retrouver. Et surtout, restez
+            à l'écoute de nos activités à venir : ateliers et visites de notre
+            distillerie au programme !{" "}
+          </p>
+
+          <form
+            className='contact-form'
+            action='enterformspreelink'
+            method='POST'
+          >
+            <input
+              type='email'
+              className='form-input'
+              placeholder='votre adresse mail'
+              name='_replyto'
+            />
+            <button type='submit' className='submit-btn'>
+              inscrivez-vous
+            </button>
+          </form>
+        </div>
+      </div>
+    </Wrapper>
+  );
 }
+
+/* Styles */
+const Wrapper = styled.section`
+  padding: 5rem 0;
+  h3 {
+    text-transform: capitalize;
+    padding-bottom: 1rem;
+  }
+  p {
+    line-height: 2;
+    max-width: 45em;
+    color: var(--textColor);
+  }
+  .contact-form {
+    width: 90vw;
+    max-width: 500px;
+    display: grid;
+    grid-template-columns: 1fr auto;
+  }
+  .form-input,
+  .submit-btn {
+    font-size: 1rem;
+    padding: 0.5rem 1rem;
+    border: 2px solid var(--headlineColor);
+  }
+  .form-input {
+    border-right: none;
+    color: var(--textColor);
+    border-top-left-radius: var(--borderRadius);
+    border-bottom-left-radius: var(--borderRadius);
+  }
+  .submit-btn {
+    border-top-right-radius: var(--borderRadius);
+    border-bottom-right-radius: var(--borderRadius);
+  }
+  .form-input::placeholder {
+    color: var(--textColor);
+    text-transform: capitalize;
+  }
+  .submit-btn {
+    background: var(--headlineColor);
+    text-transform: capitalize;
+    cursor: pointer;
+    transition: var(--transition);
+    color: var(--backgroundColor);
+  }
+  .submit-btn:hover {
+    background: var(--secondaryColor);
+  }
+  @media (min-width: 992px) {
+    .content {
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+      align-items: center;
+      gap: 8rem;
+      margin-top: 2rem;
+    }
+    p {
+      margin-bottom: 0;
+    }
+  }
+  @media (min-width: 1280px) {
+    padding: 15rem 0;
+  }
+`;
 
 export default Contact;
