36e8b32455966d41c23965a4174961166b663b73
[claks HDS] <you@example.com>
2023-01-26 13:25:04 +0000

    CartItem component

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -9,8 +9,34 @@ import AmountButtons from "./AmountButtons";
 /* React Icons */
 import { BsTrash } from "react-icons/bs";
 
-const CartItem = () => {
-  return <div>CartItem</div>;
+const CartItem = ({ id, image, name, price, amount }) => {
+  const { removeItem, toggleAmount } = useCartContext();
+
+  const increase = () => {};
+
+  const decrease = () => {};
+
+  return (
+    <Wrapper>
+      <div className='title'>
+        <img src={image} alt={name} />
+        <div>
+          <h5 className='name'>{name}</h5>
+          <h5 className='price-small'>{formatPrice(price)}</h5>
+        </div>
+      </div>
+      <h5 className='price'>{formatPrice(price)}</h5>
+      <AmountButtons amount={amount} increase={increase} decrease={decrease} />
+      <h5 className='subtotal'>{formatPrice(price * amount)}</h5>
+      <button
+        type='button'
+        className='remove-btn'
+        onClick={() => removeItem(id)}
+      >
+        <BsTrash />
+      </button>
+    </Wrapper>
+  );
 };
 
 /* Styles */
@@ -47,26 +73,9 @@ const Wrapper = styled.article`
     font-size: 0.75rem;
     margin-bottom: 0;
   }
-  .color {
-    color: var(--clr-grey-5);
-    font-size: 0.75rem;
-    letter-spacing: var(--spacing);
-    text-transform: capitalize;
-    margin-bottom: 0;
-    display: flex;
-    align-items: center;
-    justify-content: flex-start;
-    span {
-      display: inline-block;
-      width: 0.5rem;
-      height: 0.5rem;
-      background: red;
-      margin-left: 0.5rem;
-      border-radius: var(--radius);
-    }
-  }
+
   .price-small {
-    color: var(--clr-primary-5);
+    color: var(--textColor);
   }
   .amount-btns {
     width: 75px;
@@ -90,14 +99,14 @@ const Wrapper = styled.article`
     align-items: center;
     justify-content: center;
     border-radius: var(--borderRadius);
-    font-size: 0.75rem;
+    font-size: 1rem;
     cursor: pointer;
   }
   @media (min-width: 776px) {
     .subtotal {
       display: block;
       margin-bottom: 0;
-      color: var(--secondaryColor);
+      color: var(--headlineColor);
       font-weight: 400;
       font-size: 1rem;
     }
