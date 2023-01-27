a5e4d42c329e78649757b742c54413947d6a3a30
[claks HDS] <you@example.com>
2023-01-27 11:15:08 +0000

    Update PrivateRoute.js

--- src/pages/PrivateRoute.js
+++ src/pages/PrivateRoute.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { Navigate } from "react-router-dom";
+/* Context */
+import { useUserContext } from "../context/user_context";
 
-function PrivateRoute() {
-  return <div>PrivateRoute</div>;
+function PrivateRoute({ children, ...rest }) {
+  const { myUser } = useUserContext();
+
+  if (!myUser) {
+    return <Navigate to='/' />;
+  }
+  return children;
 }
 
 export default PrivateRoute;
