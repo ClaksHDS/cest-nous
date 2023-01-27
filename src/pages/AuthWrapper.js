6a00ffc751019938ace45b0ad763569cdd987011
[claks HDS] <you@example.com>
2023-01-27 11:30:29 +0000

    set up AuthWrapper

--- src/pages/AuthWrapper.js
+++ src/pages/AuthWrapper.js
@@ -1,7 +1,30 @@
 import React from "react";
+import styled from "styled-components";
+import { useAuth0 } from "@auth0/auth0-react";
 
-function AuthWrapper() {
-  return <div>AuthWrapper</div>;
-}
+const AuthWrapper = ({ children }) => {
+  const { isLoading, error } = useAuth0();
+  if (isLoading) {
+    return (
+      <Wrapper>
+        <h1>chargement en cours</h1>
+      </Wrapper>
+    );
+  }
+  if (error) {
+    return (
+      <Wrapper>
+        <h1>{error.message}</h1>
+      </Wrapper>
+    );
+  }
+  return <>{children}</>;
+};
 
+/* Styles */
+const Wrapper = styled.section`
+  min-height: 100vh;
+  display: grid;
+  place-items: center;
+`;
 export default AuthWrapper;
--- src/pages/PrivateRoute.js
+++ src/pages/PrivateRoute.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
 /* Context */
 import { useUserContext } from "../context/user_context";
 
-function PrivateRoute({ children, ...rest }) {
-  const { myUser } = useUserContext();
+function PrivateRoute({ children }) {
+  const { user } = useAuth0();
 
-  if (!myUser) {
+  if (!user) {
     return <Navigate to='/' />;
   }
   return children;
