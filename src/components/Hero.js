9193ca2b87a7bcc3325eea21a7d27e41a3b6f76f
[claks HDS] <you@example.com>
2023-01-31 10:53:42 +0000

    Update Hero content

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,13 +3,22 @@ import styled from "styled-components";
 import { Link } from "react-router-dom";
 /* Assets */
 import logo from "../assets/logo.webp";
-import bottle from "../assets/gin-bottle.webp";
+import banner from "../assets/banner.png";
 
 function Hero() {
   return (
-    <Wrapper className='section-center'>
+    <Wrapper>
       <article className='content'>
-        <h1>
+        <img
+          src={banner}
+          className='hero-img'
+          alt='banner presenting the different products of the distllery : gin, vodka, ginfusions, cold-brew, rhums'
+        />
+        <Link to='/products' className='btn hero-btn'>
+          découvrez nos spiritueux
+        </Link>
+
+        {/* <h1>
           "meilleur gin français" <br />
           <i>world gin awards 2019, 2020 et 2023</i>
           <br />
@@ -24,7 +33,7 @@ function Hero() {
           src={logo}
           alt="logo distillerie c'est nous"
           className='accent-img'
-        />
+        /> */}
       </article>
     </Wrapper>
   );
@@ -32,22 +41,39 @@ function Hero() {
 
 /* Styles */
 const Wrapper = styled.section`
-  min-height: 60vh;
-  display: grid;
-  place-items: center;
-  h1 {
-    line-height: 2;
-    text-transform: capitalize;
-    color: var(--headlineColor);
+  .content {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    text-align: center;
+    width: 100%;
+    height: 90vh; /* if you don't want it to take up the full screen, reduce this number */
+    overflow: hidden;
+    scroll-behavior: smooth;
+    margin: 0 auto;
+  }
+  .hero-img {
+    object-fit: cover;
+
+    justify-content: center;
+    overflow: hidden;
   }
   .hero-btn {
     background: var(--headlineColor);
     color: var(--backgroundColor);
     margin-top: 2rem;
+    transition: var(--transition);
   }
   .hero-btn:hover {
     background: var(--secondaryColor);
   }
+  /*  h1 {
+    line-height: 2;
+    text-transform: capitalize;
+    color: var(--headlineColor);
+  }
+  
   .img-container {
     display: none;
   }
@@ -57,26 +83,26 @@ const Wrapper = styled.section`
     margin-bottom: 2rem;
     color: var(--textColor);
     font-size: 1rem;
-  }
+  } */
   @media (min-width: 992px) {
     height: calc(100vh - 6rem);
     grid-template-columns: 1fr 1fr;
     gap: 4rem;
-    h1 {
+    /* h1 {
       margin-bottom: 2rem;
       line-height: 1.5;
     }
     p {
       font-size: 1.25rem;
-    }
+    } */
     .hero-btn {
       padding: 0.75rem 1.5rem;
       font-size: 1rem;
     }
-    .img-container {
+    /* .img-container {
       display: block;
       position: relative;
-    }
+    } */
   }
 `;
 
