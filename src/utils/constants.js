fdae10f75fe46494cde654246ae394cbae6b12c6
[claks HDS] <you@example.com>
2023-02-01 11:13:32 +0000

    Fetch products from Airtable

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,4 @@
-export const products_url = "https://course-api.com/react-store-products";
+//export const products_url = "https://course-api.com/react-store-products";
+export const products_url = "/.netlify/functions/products";
 
 export const single_product_url = `https://course-api.com/react-store-single-product?id=`;
