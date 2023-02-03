36b1f3ca72f2b5e77e7d429cdac48f24ad34b82e
[claks HDS] <you@example.com>
2023-02-03 13:41:59 +0000

    Update MapAddress.js

--- src/components/MapAddress.js
+++ src/components/MapAddress.js
@@ -1,7 +1,5 @@
 import React from "react";
 import styled from "styled-components";
-/* React Icons */
-import { BsTelephoneOutbound } from "react-icons/bs";
 
 const MapAddress = () => {
   return (
